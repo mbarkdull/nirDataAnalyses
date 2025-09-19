5a946ba3a51fbae574ce222c933e4693851a23fe
YumeKamal <Yumekamal@gmail.com>
2025-09-19 12:11:21 -0700

    Changing format of the object name

--- analyzingNirData.r
+++ analyzingNirData.r
@@ -1,14 +1,13 @@
-install.packages("googlesheets4")
 library(googlesheets4)
-install.packages("tidyverse")
 library(tidyverse)
 
-Rawnir <- read_sheet("https://docs.google.com/spreadsheets/d/1dHIhVuh-Sy2clvqRpMiu5mDXpaXXp3W5HgLbzs2rYNo/edit?usp=sharing") %>%
+rawNir <- read_sheet("https://docs.google.com/spreadsheets/d/1dHIhVuh-Sy2clvqRpMiu5mDXpaXXp3W5HgLbzs2rYNo/edit?usp=sharing") %>%
   filter(Latitude != "NULL", 
          Longitude != "NULL")
 
-ggplot(data = Rawnir, 
+ggplot(data = rawNir, 
        mapping = aes(x = Latitude, 
                      y = `Average IR`)) + 
   geom_point() + 
   geom_smooth() 
+
