8f5026a5c3c02bf4ad1dd17efd0db6519d5cf7ab
YumeKamal <Yumekamal@gmail.com>
2025-09-25 12:08:11 -0700

    added the code to access climated data & code to create a spatial object out of the ant data

--- analyzingNirData.r
+++ analyzingNirData.r
@@ -1,5 +1,12 @@
 library(googlesheets4)
 library(tidyverse)
+library(scales)
+library(lubridate)
+library(janitor)
+library(sf) 
+library(spData)
+library(terra)
+library(geodata)
 
 rawNir <- read_sheet("https://docs.google.com/spreadsheets/d/1dHIhVuh-Sy2clvqRpMiu5mDXpaXXp3W5HgLbzs2rYNo/edit?usp=sharing") %>%
   filter(Latitude != "NULL", 
@@ -11,3 +18,19 @@ ggplot(data = rawNir,
   geom_point() + 
   geom_smooth() 
 
+#create spatial data object out of my data 
+
+spatialNirData <- st_as_sf(rawNir,
+                           coords = c("Longitude", 
+                                      "Latitude"))
+spatialNirData <- st_set_crs(spatialNirData, 
+                             "+proj=longlat +datum=WGS84")
+
+#Downloading Climate Data
+
+dir.create("./data/worldclim",
+           recursive = TRUE)
+
+nirClimateData <- worldclim_global(var = "bio",
+                                   res = 10,
+                                   path = "./data/worldclim")
