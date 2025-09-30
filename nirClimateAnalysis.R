51c92877058d4e565cd69479370889cbcac5bb67
mbarkdull <mb2337@cornell.edu>
2025-09-30 08:48:30 -0700

    Adding code to check for correlations and plot with residuals.

--- nirClimateAnalysis.R
+++ nirClimateAnalysis.R
@@ -39,7 +39,9 @@ specimenData <- read_sheet("https://docs.google.com/spreadsheets/d/1dHIhVuh-Sy2c
   filter(!is.na(Latitude),
          Latitude != "NULL",
          !grepl(pattern = "\\*",
-              Latitude))
+              Latitude),
+         !is.na(`Average Visible`),
+         !is.na(`Average IR`))
 
 # Get a shapefile for the United States:
 USA <- tigris::states() 
@@ -79,24 +81,65 @@ bio <- crop(bio,
 specimenLocations$annualMeanTemperature <- terra::extract(bio[["wc2.1_30s_bio_1"]], specimenLocations)$wc2.1_30s_bio_1
 specimenLocations$maxTempWarmestMonth <- terra::extract(bio[["wc2.1_30s_bio_5"]], specimenLocations)$wc2.1_30s_bio_5
 
-# Plot annual mean temperature vs. NIR values:
+# Plot the maximum temperature of the warmest month vs. NIR values:
 ggplot(data = specimenLocations,
        mapping = aes(x = maxTempWarmestMonth,
                       y = `Average IR`)) +
   geom_point() +
-  geom_smooth() +
+  geom_smooth(method = "lm") +
   labs(x = "Maximum temperature of the warmest month",
        y = "Average near-infrared reflectance") +
   theme_bw()
 
+# Plot annual mean temperature vs. NIR values:
 ggplot(data = specimenLocations,
        mapping = aes(x = annualMeanTemperature,
                      y = `Average IR`)) +
   geom_point() +
-  geom_smooth() +
+  geom_smooth(method = "lm") +
   labs(x = "Annual mean temperature",
        y = "Average near-infrared reflectance") +
   theme_bw()
+
+# See if NIR and Vis reflectance are correlated:
+ggplot(data = specimenLocations,
+       mapping = aes(x = `Average Visible`,
+                     y = `Average IR`)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  labs(x = "Average visible reflectance",
+       y = "Average near-infrared reflectance") +
+  theme_bw()
+
+# Since they are, regress them against one another and work with the residuals for subsequent analysis:
+irByVis <- lm(data = specimenLocations, `Average IR` ~ `Average Visible`)
+summary(irByVis)
+irByVisResiduals <- residuals(irByVis)
+specimenLocations$irByVisResiduals <- irByVisResiduals
+
+# See if the residuals are now independent of Vis reflectance (they are!):
+ggplot(data = specimenLocations,
+       mapping = aes(x = `Average Visible`,
+                     y = irByVisResiduals)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  labs(x = "Annual mean temperature",
+       y = "Average near-infrared reflectance") +
+  theme_bw()
+
+# Plot residuals vs. mean temp of hottest month:
+ggplot(data = specimenLocations,
+       mapping = aes(x = maxTempWarmestMonth,
+                     y = irByVisResiduals)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  labs(x = "Maximum temperature of the warmest month",
+       y = "Average infrared reflectively\n(controlled by average visible reflectivity)") +
+  theme_bw()
+
+residualsByWarmestMonth <- lm(data = specimenLocations, irByVisResiduals ~ maxTempWarmestMonth)
+summary(residualsByWarmestMonth)
+
 
 
 
